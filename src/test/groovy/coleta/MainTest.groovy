1e9643ff311a4e22670845af96b6a48fcfa2a718
Veronica Medeiros <veronicakmedeiros@gmail.com>
2024-04-08 02:43:40 -0300

    inclusão dos testes

--- src/test/groovy/coleta/MainTest.groovy
+++ src/test/groovy/coleta/MainTest.groovy
@@ -1,40 +1,99 @@
-package coleta;
+package coleta
 
-import org.junit.Test
+import coleta.Entities.HistoricoMes
+import org.junit.jupiter.api.Test
 import static org.junit.Assert.assertEquals
-import static coleta.ObtendoDadosOuArquivos.baixarArquivosPadraoTISS.baixarPadraoTISS
 import static coleta.ObtendoDadosOuArquivos.pegarHistoricoVersoesPadraoTISS.pegarHistoricoVersoes
+import static coleta.Utilities.criarPasta.criar
+import static coleta.Utilities.baixarArquivos.baixarArquivo
+import static coleta.ObtendoDadosOuArquivos.baixarArquivosPadraoTISS.baixarPadraoTISS
 import static coleta.ObtendoDadosOuArquivos.baixarTabelaErrosEnvioANS.baixarTabelaErros
 
-import coleta.ObtendoDadosOuArquivos.baixarArquivosPadraoTISS;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-
-
-import java.io.IOException;
 
 class MainTest {
 
-    MainTest() throws IOException {
+    MainTest(){}
 
 
+    @Test
+    void criarPastaTest() {
 
+        // When:
+
+        String criarPastaTest = criar("saidasConsultasTeste", "DownloadTest")
+
+        //Than
+
+        assertEquals("./saidasConsultasTeste/DownloadTest/", criarPastaTest)
+    }
+
+
+    @Test
+    void baixarArquivoTeste() {
+
+        // Given:
+        String url = "https://www.gov.br/ans/pt-br/arquivos/assuntos/prestadores/padrao-para-troca-de-informacao-de-saude-suplementar-tiss/padrao-tiss-tabelas-relacionadas/Tabelaerrosenvioparaanspadraotiss__1_.xlsx"
+
+        // When:
+
+        boolean baixarArquivoTeste = baixarArquivo(url, "ArquivoGeradoTeste.xlsx","saidasConsultasTeste", "DownloadTest" )
+
+        //Than
+
+        assertEquals(true, baixarArquivoTeste)
+    }
+
+
+    @Test
+    void baixarArquivosPadraoTISStest() {
+
+        // When:
+
+        boolean baixarPadraoTISSteste = baixarPadraoTISS("saidasConsultasTeste", "DownloadTest")
+
+        //Than:
+
+        assertEquals(true, baixarPadraoTISSteste)
+    }
+
+
+    @Test
+    void baixarTabelaErrosEnvioANStest() {
+        
+        //When:
+
+        boolean baixarTabelaErrosTest = baixarTabelaErros("saidasConsultasTeste", "DownloadTest")
+        
+        //Than
+        
+        assertEquals(true, baixarTabelaErrosTest)
+    }
+
+
+    @Test
+    void HistoricoMesTest() {
+
+        //When
+
+        boolean historicoMes = new HistoricoMes("mar/2024", "27/03/2024", "01/04/2024")
+
+        //Than
+
+        assertEquals(true, historicoMes)
 
     }
 
-    @BeforeEach
-    void setUp() {
 
-        try {
-            Object baixarPadraoTISStest = baixarArquivosPadraoTISS.baixarPadraoTISS()
-            print(baixarPadraoTISStest)
-        }
-        catch (Exception e){
-            e.getMessage();
-        }
-    }
+    @Test
+    void pegarHistoricoVersoesPadraoTISStest() {
 
-    @AfterEach
-    void tearDown() {
+        //When
+
+        boolean pegarHistoricoVersoesTest = pegarHistoricoVersoes("saidasConsultasTeste", "DownloadTest")
+
+        //Than
+
+        assertEquals(true, pegarHistoricoVersoesTest)
     }
 }
+
